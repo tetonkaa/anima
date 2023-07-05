4e51b83f21881468d907089615cb9d6e399cf4d8
Angel <angel.l.puente@gmail.com>
2023-07-05 01:44:42 -0500

    test card css fix

--- anima/src/pages/Main/index.js
+++ anima/src/pages/Main/index.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import './main.css';
 
 
 
@@ -30,19 +30,19 @@ export default function MainPage() {
     function loaded() {
       return (
 
-          <div>
+          <>
             {tests.map((test, i) => {
               return (
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <a href="#" class="flex mainPageCards flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={test.testPic} alt=""></img>  
                 <div class="flex flex-col justify-between p-4 leading-normal">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt=""></img>  
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{test.testName}</h5>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{test.description}</p>
                   </div>  
                 </a>
               );
             })}
-          </div>
+         </> 
 
       );
     }
