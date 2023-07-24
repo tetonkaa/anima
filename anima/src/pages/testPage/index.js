cd93cd7ae217af77f1d2d8b9cae75f4060746edd
Angel <angel.l.puente@gmail.com>
2023-07-24 12:29:03 -0500

    updated state management with question load

--- anima/src/pages/testPage/index.js
+++ anima/src/pages/testPage/index.js
@@ -4,24 +4,22 @@ import { Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function TestPage(props) {
-  const [questions, setQuestions] = useState([{}]);
+  const [questions, setQuestions] = useState([]);
   const [questionsList, setQuestionsList] = useState();
   const [selectedAnswer, setSelectedAnswer] = useState(null);
 
-  console.log("hit test page", props.testId);
   async function getQuestions() {
     const { data } = await axios.get(
       props.URL + "test-questions/" + props.testId
     );
-    console.log(data);
+    setQuestions(data);
     return data;
   }
   useEffect(() => {
-    getQuestions().then((data) => setQuestions(data));
-  }, []);
+    getQuestions();
+  }, [props.testId]);
 
   useEffect(() => {
-    setQuestionsList({ questions }.questions.docs);
     console.log(questions);
   }, [questions]);
 
@@ -30,14 +28,17 @@ export default function TestPage(props) {
     console.log("Selected Answer:", event.target.value);
   };
 
-  return (
+  return questions.length > 0 ? (
     <>
-      {questions.map((questionItem, i) => { // Rename the variable to questionItem
+      {questions.map((questionItem, i) => {
         return (
-          <div key={questionItem._id} className="flex mainPageCards flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+          <div
+            key={questionItem._id}
+            className="flex mainPageCards flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+          >
             <div className="flex flex-col justify-between p-4 leading-normal">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {questionItem.question} {/* Use questionItem instead of questions */}
+                {questionItem.question}
               </h5>
               <ul>
                 {questionItem.answerChoices.map((answer) => (
@@ -45,11 +46,15 @@ export default function TestPage(props) {
                     <label>
                       <input
                         type="radio"
-                        name={`answer_${questionItem._id}`} 
+                        name={`answer_${questionItem._id}`}
                         value={answer.Answer}
-                        onChange={(event) => handleAnswerChange(event, questionItem._id)}
+                        onChange={(event) =>
+                          handleAnswerChange(event, questionItem._id)
+                        }
                       />
-                      <span>{answer.Answer} - Score: {answer.Score}</span>
+                      <span>
+                        {answer.Answer} - Score: {answer.Score}
+                      </span>
                     </label>
                   </li>
                 ))}
@@ -59,5 +64,29 @@ export default function TestPage(props) {
         );
       })}
     </>
+  ) : (
+    <div>
+      {/* <h1 className=" loader animate__animated animate__pulse animate__infinite 	infinite"> Loading...</h1> */}
+      <svg
+        className="loaderSVG"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="12" cy="12" r="3" />
+        <g>
+          <circle cx="4" cy="12" r="3" />
+          <circle cx="20" cy="12" r="3" />
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            calcMode="spline"
+            dur="1s"
+            keySplines=".36,.6,.31,1;.36,.6,.31,1"
+            values="0 12 12;180 12 12;360 12 12"
+            repeatCount="indefinite"
+          />
+        </g>
+      </svg>
+    </div>
   );
 }
