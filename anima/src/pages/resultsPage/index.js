6c63730b7894ec1df989010317ce0dc930e752d8
Angel <angel.l.puente@gmail.com>
2023-08-13 00:17:17 -0500

    updated image rendering on results page

--- anima/src/pages/resultsPage/index.js
+++ anima/src/pages/resultsPage/index.js
@@ -8,24 +8,55 @@ export default function ResultsPage(props) {
     userResult: props.userResult,
   };
 
-  console.log(resultParam);
   async function getTestResults() {
     const { data } = await axios.post(
       props.URL + "result/" + thisTest,
       resultParam
     );
     setRenderedResult(data);
-    console.log(renderedResult);
+    return data;
   }
   useEffect(() => {
     getTestResults();
   }, []);
 
-  {
+  useEffect(() => {
+    console.log(renderedResult);
+  }, [renderedResult]);
+
+  function loaded() {
     return (
       <>
-        <h1>Results Page</h1>
+        <img src={renderedResult.image} />
       </>
     );
   }
+
+  return renderedResult ? (
+    loaded()
+  ) : (
+    <div>
+      {/* <h1 className=" loader animate__animated animate__pulse animate__infinite 	infinite"> Loading...</h1> */}
+      <svg
+        className="loaderSVG"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="12" cy="12" r="3" />
+        <g>
+          <circle cx="4" cy="12" r="3" />
+          <circle cx="20" cy="12" r="3" />
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            calcMode="spline"
+            dur="1s"
+            keySplines=".36,.6,.31,1;.36,.6,.31,1"
+            values="0 12 12;180 12 12;360 12 12"
+            repeatCount="indefinite"
+          />
+        </g>
+      </svg>
+    </div>
+  );
 }
