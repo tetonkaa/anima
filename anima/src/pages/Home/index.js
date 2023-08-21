617e2e911ec54136ff64c5a6711a562933f7cba8
Angel <angel.l.puente@gmail.com>
2023-08-20 21:26:55 -0500

    main page css

--- anima/src/pages/Home/index.js
+++ anima/src/pages/Home/index.js
@@ -2,6 +2,7 @@ import "./main.css"
 export default function HomePage() {
 return(
     <div class="homePageContainer">
+      <div class="backgroundEffect"></div>
       <a>Welcome to Anima</a>
     </div>
 )
