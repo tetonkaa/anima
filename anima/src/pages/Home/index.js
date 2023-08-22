651ed363b4f475b34aad483a634d1487a0c6d91d
Angel <angel.l.puente@gmail.com>
2023-08-22 14:06:44 -0500

    background for modal

--- anima/src/pages/Home/index.js
+++ anima/src/pages/Home/index.js
@@ -7,7 +7,7 @@ export default function HomePage() {
     <div class="homePageContainer">
       <div class="backgroundEffect"></div>
       <a>
-        Define your<br></br>selection
+        Define<br></br>Yourself
       <p>A personality test generator.<br/> Create and share assessments with <br/>
          non-numerical results.</p>
       </a>
