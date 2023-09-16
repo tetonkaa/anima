8d34c9849cd05c6a829d2e94683fcfccebe2fa17
“julian <badjab326@gmail.com>
2023-09-15 20:24:39 -0700

    Update: initialized question create logic rework

--- anima/src/components/questionCreateComponent/questionCreateComponent.js
+++ anima/src/components/questionCreateComponent/questionCreateComponent.js
@@ -1,69 +1,23 @@
 import React, { useState } from "react";
-import axios from "axios";
 import "./main.css";
 
-export default function QuestionCreateComponent(URL, props,addQuestion) {
-  const [createdQuestion, setCreatedQuestion] = useState({
-    question: "",
-    answerChoices: [
-      { Label: "A", Score: 1, Answer: "" },
-      { Label: "B", Score: 1, Answer: "" },
-      { Label: "C", Score: 1, Answer: "" },
-      { Label: "D", Score: 1, Answer: "" },
-    ],
-    testId: localStorage.getItem("newTestId"),
-  });
-
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   try {
-  //     const response = await axios.post(
-  //       URL.URL + "questions/add-question",
-  //       createdQuestion
-  //     );
-  //     console.log("Question post successful", response);
-  //   } catch (error) {
-  //     console.error("Error posting question", error);
-  //   }
-  // };
-
+export default function QuestionCreateComponent({
+  handleQuestionInput,
+  handleAnswerChoiceChange,
+  handleCreateQuestion,
+  createdQuestion,
+  ...props
+}) {
   
-  const handleQuestionInput = (event) => {
-    const { name, value } = event.target;
-    setCreatedQuestion((previousData) => ({
-      ...previousData,
-      [name]: value,
-    }));
-  };
-  
-  // console.log(createdQuestion)
-  console.log(URL.URL);
-  
-  const handleAnswerChoiceChange = (index, field, value) => {
-    const updatedAnswerChoices = [...createdQuestion.answerChoices];
-    updatedAnswerChoices[index][field] = value;
-    
-    setCreatedQuestion((previousData) => ({
-      ...previousData,
-      answerChoices: updatedAnswerChoices,
-    }));
-  };
-  
-  const handleCreateQuestion = (e) =>{
-    // addQuestion(createdQuestion)
-  }
   return (
     <div className="questionCreateContainer">
       <h1>Question</h1>
-      <form >
+      <form>
         <input
           type="text"
           name="question"
           placeholder="Enter a question"
-          value={createdQuestion.question}
           onChange={handleQuestionInput}
-          readonly
         />
         <div>
           {createdQuestion.answerChoices.map((choice, index) => (
@@ -94,7 +48,9 @@ export default function QuestionCreateComponent(URL, props,addQuestion) {
             </div>
           ))}
         </div>
-        <button type="button" onClick={handleCreateQuestion}>Submit</button>
+        <button type="button" onClick={handleCreateQuestion}>
+          Submit
+        </button>
       </form>
     </div>
   );
--- anima/src/pages/questionCreate/index.js
+++ anima/src/pages/questionCreate/index.js
@@ -3,21 +3,31 @@ import axios from "axios";
 import "./main.css";
 import QuestionCreateComponent from "../../components/questionCreateComponent/questionCreateComponent";
 
-
 export default function QuestionCreate(props) {
-const [questionForms, setQuestionForms] = useState([<QuestionCreateComponent URL={props.URL} key={0} />])
-const [createdQuestions, setCreatedQuestions] = useState([]);
+  const [createdQuestions, setCreatedQuestions] = useState([]);
+  const [createdQuestion, setCreatedQuestion] = useState({
+    question: "",
+    answerChoices: [
+      { Label: "A", Score: 1, Answer: "" },
+      { Label: "B", Score: 1, Answer: "" },
+      { Label: "C", Score: 1, Answer: "" },
+      { Label: "D", Score: 1, Answer: "" },
+    ],
+    testId: localStorage.getItem("newTestId"),
+  });
 
-// const addQuestion = (createdQuestion) => {
-//   // Add the question to the createdQuestions array
-//   setCreatedQuestions({...createdQuestions, createdQuestion});
-// };
+  // const addQuestion = (createdQuestion) => {
+  //   // Add the question to the createdQuestions array
+  //   setCreatedQuestions({...createdQuestions, createdQuestion});
+  // };
 
-
-let handleAddForm = (e) => {
-  e.preventDefault()
-  setQuestionForms([...questionForms,<QuestionCreateComponent key={questionForms.length}/>]);
-}
+  let handleAddForm = (e) => {
+    e.preventDefault();
+    setQuestionForms([
+      ...questionForms,
+      <QuestionCreateComponent key={questionForms.length} />,
+    ]);
+  };
 
   // const handleFormSubmit = async (event) => {
   //   event.preventDefault();
@@ -33,7 +43,6 @@ let handleAddForm = (e) => {
   //   }
   // };
 
-
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -43,23 +52,68 @@ let handleAddForm = (e) => {
 
       // Use axios to post the questions to the server here
       // ...
-
     } catch (error) {
       console.error("Error posting questions", error);
     }
   };
 
+  console.log(createdQuestions);
 
-  console.log(createdQuestions)
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
 
+  //   try {
+  //     const response = await axios.post(
+  //       URL.URL + "questions/add-question",
+  //       createdQuestion
+  //     );
+  //     console.log("Question post successful", response);
+  //   } catch (error) {
+  //     console.error("Error posting question", error);
+  //   }
+  // };
+
+  const handleQuestionInput = (event) => {
+    const { name, value } = event.target;
+    setCreatedQuestion((previousData) => ({
+      ...previousData,
+      [name]: value,
+    }));
+  };
+
+  // console.log(createdQuestion)
+
+  const handleAnswerChoiceChange = (index, field, value) => {
+    const updatedAnswerChoices = [...createdQuestion.answerChoices];
+    updatedAnswerChoices[index][field] = value;
+
+    setCreatedQuestion((previousData) => ({
+      ...previousData,
+      answerChoices: updatedAnswerChoices,
+    }));
+  };
+
+  const handleCreateQuestion = (e) => {
+    // addQuestion(createdQuestion)
+  };
+  const [questionForms, setQuestionForms] = useState([
+    <QuestionCreateComponent
+      handleQuestionInput={handleQuestionInput}
+      handleAnswerChoiceChange={handleAnswerChoiceChange}
+      handleCreateQuestion={handleCreateQuestion}
+      createdQuestion={createdQuestion}
+      key={0}
+    />,
+  ]);
   return (
     <div class="questionAddPageContainer">
-      <QuestionCreateComponent URL={props.URL} />
       <form>
         {questionForms}
         <button onClick={handleAddForm}>ADD QUESTION</button>
       </form>
-      <button type="submit" onClick={handleFormSubmit}>Finish Questions</button>
+      <button type="submit" onClick={handleFormSubmit}>
+        Finish Questions
+      </button>
     </div>
   );
 }
