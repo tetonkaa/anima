86decafd8d6d6fd18a4e88cffccabf3368452582
Angel <angel.l.puente@gmail.com>
2023-11-26 13:14:06 -0600

    update question map to render questions individually

--- anima/src/pages/testPage/index.js
+++ anima/src/pages/testPage/index.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from "react";
 import { ReactComponent as Loading } from "../../assets/loading.svg";
 import { toast, ToastContainer } from 'react-toastify';
 export default function TestPage(props) {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [thisTest, setThisTest] = useState(
     localStorage.getItem("currentTestId")
   ); //define testId from local storage
@@ -15,7 +16,7 @@ export default function TestPage(props) {
     userAnswers: {},
     user: props.user ? props.user.uid : "guest",
   }); //define user answers as object, using thisTest as key and answers as object for value
-
+  const [animateQuestion, setAnimateQuestion] = useState(true);
   const [answeredQuestions, setAnsweredQuestions] = useState({});
   const [inviteLink, setInviteLink] = useState();
   const urlTestId = window.location.hash.replace("#", "");
@@ -40,14 +41,25 @@ export default function TestPage(props) {
   },[]);
 
 
-  const handleAnswerValue = (event, answerValue) => {
+  const handleAnswerValue = (event, answer) => {
     const questionId = event.target.name;
-    const answerScore = event.target.value;
-    const answerLabel = event.target.id;
-
-    userAnswers.userAnswers[questionId] = { [answerLabel]: answerScore };
-
-    setAnsweredQuestions({ ...answeredQuestions, [questionId]: true });
+    const answerLabel = answer.Label;
+  
+    setUserAnswers((prevUserAnswers) => {
+      const updatedUserAnswers = {
+        ...prevUserAnswers,
+        userAnswers: {
+          ...prevUserAnswers.userAnswers,
+          [questionId]: { [answerLabel]: answer.Score },
+        },
+      };
+      return updatedUserAnswers;
+    });
+  
+    setAnsweredQuestions((prevAnsweredQuestions) => ({
+      ...prevAnsweredQuestions,
+      [questionId]: true,
+    }));
   };
   const handleShare = () => {
     console.log(inviteLink);
@@ -65,6 +77,15 @@ export default function TestPage(props) {
     toast.success(`Link copied: ${copyText.value}`);
   };
 
+  const handleNextQuestion = () => {
+    setAnimateQuestion(true);
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handleAnimationEnd = () => {
+    setAnimateQuestion(false);
+  };
+
   async function handleAnswerSubmit() {
     const updateProps = (response) => {
       props.setUserResult(response);
@@ -76,18 +97,24 @@ export default function TestPage(props) {
   }
 
   return questions.length > 0 ? (
-    <div class="testPageContainer">
+    <div className="testPageContainer">
       <ToastContainer />
+  
       <input type="text" value={inviteLink} id="linkShare"></input>
-      <button class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" type="button" onClick={handleShare}>
+      <button
+        className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        type="button"
+        onClick={handleShare}
+      >
         Share This Test
       </button>
-            {questions.map((questionItem, i) => {
-        return (
-          <div>
+  
+      {questions.map((questionItem, i) => {
+        return i === currentQuestionIndex ? (
+          <div key={questionItem._id}>
             <div
-              key={questionItem._id}
-              className="flex testPageCards flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+              className={`flex testPageCards flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 ${ animateQuestion ? "fadeIn" : ""}`}
+              onAnimationEnd={handleAnimationEnd}
             >
               <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -102,12 +129,9 @@ export default function TestPage(props) {
                           name={questionItem._id}
                           value={answer.Score}
                           id={answer.Label}
-                          onClick={(event) =>
-                            // handleAnswerChange(event, questionItem._id)
-                            handleAnswerValue(event, answer)
-                          }
+                          onClick={(event) => handleAnswerValue(event, answer)}
                         />
-                        <span class="answer">{answer.Answer}</span>
+                        <span className="answer">{answer.Answer}</span>
                       </label>
                     </li>
                   ))}
@@ -115,20 +139,20 @@ export default function TestPage(props) {
               </div>
             </div>
           </div>
-        );
+        ) : null;
       })}
-      <button
-        type="button"
-        class="d3Button text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        disabled={Object.keys(answeredQuestions).length !== questions.length}
-        onClick={handleAnswerSubmit}
-      >
-        Submit
-      </button>
+  
+  <button
+  type="button"
+  className="d3Button text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+  disabled={!answeredQuestions[questions[currentQuestionIndex]._id]}
+  onClick={currentQuestionIndex === questions.length - 1 ? handleAnswerSubmit : handleNextQuestion}
+>
+  {currentQuestionIndex === questions.length - 1 ? 'Submit' : 'Next Question'}
+</button>
     </div>
   ) : (
     <div className="loadingSvg">
-      {/* <h1 className=" loader animate__animated animate__pulse animate__infinite 	infinite"> Loading...</h1> */}
       <Loading />
     </div>
   );
